09b3d2c19d6feaf938960a47711fa3e9b1bdcf56
WTV26 <renato26faria@gmail.com>
2025-10-02 22:45:43 -0400

    Implement event index for students and organizers separately and add event routes for CRUD

--- Sprint2/app/controllers/eventsController.js
+++ Sprint2/app/controllers/eventsController.js
@@ -2,13 +2,52 @@
 const { PrismaClient } = require("generated-prisma/client");
 const prisma = new PrismaClient();
 
-const event_index = (req, res) => {
+//This event index is specifically for the user facing event index. See organizer controller for the organizer event index
+const event_index_student = async (req, res) => {
+	try {
+		const events = await prisma.event.findMany({
+			where: {published: true},
+			orderBy: {startsAt: "asc"},
+			include:{
+				organizer: {select: {id: true, firstName:true, lastName:true}},
+				_count: {select:{tickets:true}}
+			}
+		})
+		res.json(events)
+	} catch (error) {
+		res.status(500).json({error: 'Failed to fetch events'})
+	}
+};
 
-}
+const event_index_organizer = async (req, res) => {
+	try {
+		const organizerId = req.params.id; 
+		const events = await prisma.event.findMany({
+			where: {organizerId},
+			orderBy: {createdAt: "desc"},
+		});
+		res.json(events);		
+	} catch (error) {
+		res.status(500).json({error: 'Failed to fetch your created events'});
+	}
+};
 
-const event_details = () => {
+const event_details = async (req, res) => {
+	try {
+		const event = await prisma.event.findUnique({
+			where: {id: req.params.id},
+			include:{
+				title,
+				details,
 
-}
+
+			}
+		})
+	} catch (error) {
+		
+	}
+
+};
 
 const event_create = async (req, res) => {
 	try {
@@ -32,9 +71,19 @@ const event_create = async (req, res) => {
 	}
 };
 
-const event_delete = () => {
-
-}
+const event_delete = async (req, res) => {
+	try {
+		const event = await prisma.event.delete(
+			{
+				where: {id: req.params.id}
+			}
+		);
+		res.json({message: "Event deleted"});		
+	} catch (error) {
+		console.error(error);
+		res.status(400).json({error: "Event deletion failed"})
+	}
+};
 
 const event_publish = async (req, res) => {
 	try {
@@ -44,16 +93,32 @@ const event_publish = async (req, res) => {
 				data: {published: true},
 			}
 		);
+		res.json(event)
 	} catch (error) {
 		console.error(error)
 		res.status(400).json({error: "Failed to publish the event"});
 	}
 };
 
+const event_unpublish = async (req, res) => {
+	try {
+		const event = await prisma.event.update(
+			{
+				where: {id: req.params.id},
+				data: {published: false},
+			}
+		);
+	} catch (error) {
+		console.error(error)
+		res.status(400).json({error: "Failed to unpublish the event"});
+	}
+};
 module.exports = {
-	event_index,
+	event_index_student,
+	event_index_organizer,
 	event_details,
 	event_create,
 	event_delete,
-	event_publish
+	event_publish,
+	event_unpublish
 };
--- Sprint2/app/routes/events.public.js
+++ Sprint2/app/routes/events.public.js
@@ -2,7 +2,7 @@ const express = require('express');
 const eventsController = require('../controllers/eventsController');
 const router = express.Router();
 
-router.get('/', eventsController.event_index);
-router.post('/',eventsController.event_create);
-router.patch('/:id/publish', eventsController.event_publish);
+router.get('/', eventsController.event_index_student);
+router.get('/:id', eventsController.event_details)
+
 module.exports = router;
