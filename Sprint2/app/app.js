a9531a49a7028941b249bba4d57e584f36bf74c3
WTV26 <renato26faria@gmail.com>
2025-10-02 22:44:34 -0400

    Fix import statement for PrismaClient and update event routes

--- Sprint2/app/app.js
+++ Sprint2/app/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
-import { PrismaClient } from "generated-prisma/client";
+const { PrismaClient } = require("generated-prisma/client");
 const crypto = require("crypto");
 const QRCode = require('qrcode');
 
-const eventRoutes = require('./routes/events')
+const eventRoutes = require('./routes/events.public')
 
 const app = express();
 const prisma = new PrismaClient();
@@ -12,12 +12,14 @@ const prisma = new PrismaClient();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended : true}));
+app.use(express.json());
 app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 
 app.listen(3000);
 
-app.use('/events',eventRoutes);
+app.use("/events", require("./routes/events.public"));
+app.use("/organizers", require("./routes/events.organizer"));
 
 //404 error page
 app.use((req, res) => {
