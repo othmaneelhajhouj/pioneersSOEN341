3e0ed8372cd852b12a1d97d95905b6754bf55d58
WTV26 <renato26faria@gmail.com>
2025-10-02 21:38:33 -0400

    Added event create and publish backend api

--- Sprint2/app/controllers/eventsController.js
+++ Sprint2/app/controllers/eventsController.js
@@ -1,5 +1,5 @@
 // event_index, event_details, event_create, event_delete, event_publish
-import { PrismaClient } from "generated-prisma/client";
+const { PrismaClient } = require("generated-prisma/client");
 const prisma = new PrismaClient();
 
 const event_index = (req, res) => {
@@ -10,17 +10,45 @@ const event_details = () => {
 
 }
 
-const event_create = () => {
-
-}
+const event_create = async (req, res) => {
+	try {
+		const {title, description, startsAt, endsAt, location, type, capacity, organizerId} = req.body;
+		const event = await prisma.event.create({
+			data: {
+				title,
+				description,
+				startsAt,
+				endsAt,
+				location,
+				type,
+				capacity,
+				organizerId,
+			},
+		});
+		res.status(201).json(event);
+	} catch (error) {
+		console.error(error);
+		res.status(400).json({error:"Event creation failed", details: error.message});
+	}
+};
 
 const event_delete = () => {
 
 }
 
-const event_publish = () => {
-
-}
+const event_publish = async (req, res) => {
+	try {
+		const event = await prisma.event.update(
+			{
+				where: {id: req.params.id},
+				data: {published: true},
+			}
+		);
+	} catch (error) {
+		console.error(error)
+		res.status(400).json({error: "Failed to publish the event"});
+	}
+};
 
 module.exports = {
 	event_index,
