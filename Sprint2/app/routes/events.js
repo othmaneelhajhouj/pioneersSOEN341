200aab6aed0abf8d11d5a899a25646d69ea958a0
WTV26 <renato26faria@gmail.com>
2025-10-02 21:40:36 -0400

    Set event routes: event creation and publishing endpoints

--- Sprint2/app/routes/events.js
+++ Sprint2/app/routes/events.js
@@ -3,6 +3,6 @@ const eventsController = require('../controllers/eventsController');
 const router = express.Router();
 
 router.get('/', eventsController.event_index);
-
-
+router.post('/',eventsController.event_create);
+router.patch('/:id/publish', eventsController.event_publish);
 module.exports = router;
