cda400513cdbbe84771209bba9883ed2a3e77e16
othmane el hajhouj <othmaneelhajhouj@gmail.com>
2025-10-08 21:34:34 -0400

    mount admin routes, remove server.ts, add req.user types, fix migrations for GET testing

--- Sprint2/app/routes/events.organizer.js
+++ Sprint2/app/routes/events.organizer.js
@@ -6,4 +6,5 @@ router.get('/:OrganizerId/events', eventsController.event_index_organizer);
 router.post('/:OrganizerId/events',eventsController.event_create);
 router.patch('/events/:eventId/publish', eventsController.event_publish);
 router.patch('/events/:eventId/unpublish', eventsController.event_unpublish);
-router.delete('/events/:eventId',eventsController.event_delete)
+router.delete('/events/:eventId',eventsController.event_delete);
+module.exports = router;
