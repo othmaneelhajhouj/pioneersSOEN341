0e0ac483ba8ffa1c9a12b7c2a62dd837e458a6e5
Renato <49315589+WTV26@users.noreply.github.com>
2025-11-18 21:44:11 -0500

    Remove REPLICATE_API_TOKEN validation
    Remove check for REPLICATE_API_TOKEN environment variable
    Causes tests to fail. Reverting to original setup.

--- app/controllers/eventsController.js
+++ app/controllers/eventsController.js
@@ -12,9 +12,6 @@ const { existsSync } = require("fs");
 const path = require("path");
 const Replicate = require("replicate");
 
-if (!process.env.REPLICATE_API_TOKEN) {
-  throw new Error('REPLICATE_API_TOKEN environment variable is required');
-}
 const replicate = new Replicate();
 
 /**
